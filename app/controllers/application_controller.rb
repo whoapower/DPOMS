e6ade0f536c357ccb1872ca30d299e34bc852e9b
direct <direct@localhost.localdomain>
2011-07-08 12:39:32 -0500

    Added helpers, shared views, and other miscellany.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  # Render a templated "Not Found" page via app/views/custom_pages/404.html.haml
+  def render_404
+    render 'custom_pages/404', :status => :not_found, :layout => 'one_column'
+    return true
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+  # Use within views to set the page's HTML title
+  def title(text)
+    content_for(:title) { text }
+  end
+
+  # Use within views to set the page's HTML meta description
+  def description(text)
+    content_for(:description) { text }
+  end
+
+  # Displays all flash messages in appropriately named divs for easy styling
+  def flash_messages
+    messages = flash.keys.collect { |key| flash_message(key, flash[key]) }
+    content_tag(:div, messages.to_s.html_safe, :id => "flash-messages")
+  end
+
+  # Format a single flash message
+  def flash_message(type, message)
+    content_tag(:div, :class => "flash-message #{type.to_s}") do
+      content_tag :p, message
+    end
+  end
+
 end
